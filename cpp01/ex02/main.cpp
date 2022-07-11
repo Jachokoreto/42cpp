fe30a479217c54507ee519a385af192ce708328c
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2022-07-11 18:32:04 +0800

    Done cpp01/ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 18:21:02 by jatan             #+#    #+#             */
-/*   Updated: 2022/07/11 18:26:17 by jatan            ###   ########.fr       */
+/*   Updated: 2022/07/11 18:31:43 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,12 @@ int main(void)
 	string *sPtr = &str;
 	string &sRef = str;
 
-	cout << "Printing memory address..." << endl;
-	cout << "Memory address of str : " << &str << endl;
-	cout << "Memory address of sPtr : " << &str << endl;
-	cout << "Memory address of sRef : " << &str << endl;
+	cout << "Printing memory address..." << endl
+		 << "Memory address of str : " << &str << endl
+		 << "Memory address of sPtr : " << &sPtr << endl
+		 << "Memory address of sRef : " << &sRef << endl << endl
+		 << "Printing value..." << endl
+		 << "Value of str : " << str << endl
+		 << "Value of sPtr : " << *sPtr << endl
+		 << "Value of sRef : " << sRef << endl;
 }
