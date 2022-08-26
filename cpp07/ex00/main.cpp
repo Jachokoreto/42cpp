341ee80703c2a8b3d0871aff6b8b90ad52e3db8b
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2022-08-26 15:07:40 +0800

    Update main.cpp for cpp07/ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -26,7 +26,19 @@ int main(void)
 	msg::info("Return second one when equal, func(a, b)");
 	a = 10;
 	b = 10;
-	int &c = min(a, b);
-	std::cout << "ret\t: " << &c << std::endl
+	int &bTmp = min(a, b);
+	std::cout << "ret\t: " << &bTmp << std::endl
 			  << "b\t: " << &b << std::endl;
+
+	msg::info("Test from PDF");
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
