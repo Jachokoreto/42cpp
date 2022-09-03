f2b14fffae283f58b8055fdb1815d89bda7e3ea4
Jachokoreto <jatan@student.42kl.edu.my>
2022-09-04 05:07:05 +0800

    Update

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,12 +6,26 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:18:41 by jatan             #+#    #+#             */
-/*   Updated: 2022/07/13 15:28:24 by jatan            ###   ########.fr       */
+/*   Updated: 2022/09/04 05:06:36 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
+
+/**
+ * What is fixed point?
+ * - In computing, fixed-point refers to a method of representing fractional
+ * (non-integer) numbers by storing a fixed number of digits of their fractional part.
+ * - So, we can set our desired amount of bits to decide its precision
+ *
+ * Fixed point
+ * - used to represent a limited range of values
+ *
+ * vs Floating point
+ * - used to represent a wide range of values
+ *
+ */
 
 int main(void)
 {
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 14:56:36 by jatan             #+#    #+#             */
-/*   Updated: 2022/07/13 21:12:44 by jatan            ###   ########.fr       */
+/*   Updated: 2022/09/04 04:58:29 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,25 @@ using std::ostream;
 class Fixed
 {
 
-  public:
+public:
 	Fixed(void);
-	Fixed(const int value);
-	Fixed(const float value);
+	Fixed(const int value);	  // * integer constructor
+	Fixed(const float value); // * floating point constructor
 	Fixed(const Fixed &fixed);
 	Fixed &operator=(const Fixed &fixed);
 	~Fixed(void);
 
-	float toFloat(void) const;
-	int toInt(void) const;
+	float toFloat(void) const; // * Int to Fixed point
+	int toInt(void) const;	   // * Fixed point to Int
 
 	int getRawBits(void) const;
 	void setRawbits(int const raw);
 
-  private:
+private:
 	int _value;
 	static const int _fracBits;
 };
 
-ostream &operator<<(ostream &o, Fixed const &rhs);
+ostream &operator<<(ostream &o, Fixed const &rhs); // * << operator overload
 
 #endif /* ********************************************************** Fixed_H */
