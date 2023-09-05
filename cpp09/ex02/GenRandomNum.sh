96c39a1e74b16ee35ce873e8d6c19c88222fee64
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-09-05 22:48:57 +0800

    feat: add a script to generate random number

---
+++ cpp09/ex02/GenRandomNum.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This script generates a list of Random Number
+#Author: Eduonix
+#Date: Oct 2015
+
+# DIFF=$(($2-$1+1))
+# RANDOM=$$
+# for i in `seq $3`
+# do
+# 	R=$(($(($RANDOM%$DIFF))+$1))
+# 	echo $R
+# done
+
+awk -v loop=$2 -v range=$1 'BEGIN{
+  srand()
+  do {
+    numb = 1 + int(rand() * range)
+    if (!(numb in prev)) {
+       print numb
+       prev[numb] = 1
+       count++
+    }
+  } while (count<loop)
+}' | tr '\n' ' '
